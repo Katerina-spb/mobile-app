c407ac1ce044a3c02e9ff9b158073ef61b329a65
katerina-spb <katespb98@gmail.com>
2021-07-20 12:52:22 -0500

    Created API calls

---
+++ apiMaster.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const url = 'https://w-data.netlify.app/.netlify/functions/app';
+
+const apiMaster = {
+  getForecast: (city) => {
+    return axios.get(`${url}/forecast`, { params: { city: city }});
+  },
+  getWeather: (city) => {
+    return axios.get(`${url}/weather`, { params: { city: city }});
+  },
+}
+
+export default apiMaster;
